d8d20849bccffde29afec4e35aae0494e31060c9
RobertoZeppilli <robzep171@gmail.com>
2021-04-27 16:22:47 +0200

    vars inside the if..else

--- js/script.js
+++ js/script.js
@@ -12,26 +12,19 @@ document.getElementById("age").innerHTML = passengerAge + " anni";
 var ticketPrice = (kmNumber * 0.21);
 // console.log(ticketPrice);
 
-// 3.1. Var of normal/full price
-var fullPrice = ticketPrice.toFixed(2);
-
-// 4. Var with sale on kids and over-65
-var kidsTicket = (ticketPrice - ((ticketPrice * 20) / 100)).toFixed(2);
-// console.log(kidsTicket);
-
-var over65Ticket = (ticketPrice - ((ticketPrice * 40) / 100)).toFixed(2);
-// console.log(over65Ticket);
-
-// 5. Final price in relationship to specific case
+// 4. Final price in relationship to specific case
 if (passengerAge < 18) {
+    var kidsTicket = (ticketPrice - ((ticketPrice * 20) / 100)).toFixed(2);
     document.getElementById("kids").innerHTML = kidsTicket + " &euro;";
     document.getElementById("over-65").innerHTML = "Il passeggero è minorenne";
     document.getElementById("full-price").innerHTML = "Il passeggero è minorenne";
 } else if (passengerAge > 65) {
+    var over65Ticket = (ticketPrice - ((ticketPrice * 40) / 100)).toFixed(2);
     document.getElementById("over-65").innerHTML = over65Ticket + " &euro;";
     document.getElementById("kids").innerHTML = "Il passeggero è over-65";
     document.getElementById("full-price").innerHTML = "Il passeggero è over-65";
 } else {
+    var fullPrice = ticketPrice.toFixed(2);
     document.getElementById("full-price").innerHTML = fullPrice + " &euro;";
     document.getElementById("kids").innerHTML = "Il biglietto avrà prezzo pieno";
     document.getElementById("over-65").innerHTML = "Il biglietto avrà prezzo pieno";
